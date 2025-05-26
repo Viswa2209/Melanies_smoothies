ef9ac85c981da85858b2730b9b75a93e449d12eb
Viswa2209 <vishwanathvicky55@gmail.com>
2025-05-26 13:29:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
-from snowflake.snowpark.functions import when_matched
+from snowflake.snowpark.functions import col, when_matched
 
 helpful_links = [
     "https://docs.streamlit.io",
